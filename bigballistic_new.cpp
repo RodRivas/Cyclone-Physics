28bd5a7ae20716fdc74a29efbfb739c656040ba8
Rodrigo Gonzales <rrivas2009@gmail.com>
2017-04-21 12:58:06 -0300

    Update and rename ballistic_new.cpp to bigballistic_new.cpp

--- bigballistic_new.cpp
+++ bigballistic_new.cpp
@@ -3,7 +3,7 @@
 that allows the user to choose some angles (in x and z axis) by pressing the numbers 5, 6, 7 and 8 before the shoot
 */
 
-/* (PT)  Versão modificada do arquivo "ballistic.cpp" para implementação de uma 
+/* (PT)  Versão modificada do arquivo "bigballistic.cpp" para implementação de uma 
 guinada (nos eixos x e z) no angulo de tiro. Acréscimo das funções: angulo() e setAngulo(). 
 Através das teclas "5,6,7 ou 8" o usuário modifica os valores da variável sinCos, 
 implementando seno e cosseno (na variável "position" dentro da função render()), antes de o tiro ser disparado.
